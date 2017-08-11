cf88863734a7603f2b89870db6f9593362cd917a
Lukas Folkman <folkmanl@bs-hpd203.ethz.ch>
2017-08-11 18:21:40 +0200

    run all fix

--- run_all.sh
+++ run_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 python dataset.py URLs_to_GDSC_datasets.txt
+bash prepare_KBMTL.sh
+
 for analysis in "KEEPK" "SAMPLE" #"FULL"
 do
 	cp test/${analysis}_config.yaml config.yaml
 	python prepare.py config.yaml
 	python exp.py config.yaml
-	bash prepare_KBMTL.sh
 	matlab -nodisplay -nosplash -nodesktop -r "run_KBMTL"
 	python results.py config.yaml > ${analysis}_results.txt
 done
-jupyter notebook plots.ipynb
+
+#jupyter notebook plots.ipynb
 
